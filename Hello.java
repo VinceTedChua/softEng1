a8e5d93738cc922dba4934655f30e14fa4ae5a76
VinceTedChua <142372312+VinceTedChua@users.noreply.github.com>
2023-08-16 12:27:38 +0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello{
+   public static void main(String[] args){
+      System.out.println("Hello, Software Engineering Course!");
+      System.out.println("My name is CHUA, Vince Ted Matthew!");
+
+   }
+}
